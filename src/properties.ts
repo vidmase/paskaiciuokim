330bed4d05bd8e9b0463bf6d595ee4d25694afd3
vidmase <123352953+vidmase@users.noreply.github.com>
2023-11-07 00:38:22 +0000

    Update properties.ts

--- src/properties.ts
+++ src/properties.ts
@@ -1,55 +1,54 @@
 export const BUY_TO_LET_INPUT = {
     propertyValue: {
-      label: 'Turto vertė',
-      description: 'The value of the property being purchased',
+        label: 'Turto vertė',
+        description: 'The value of the property being purchased',
     },
     loanToValue: {
-      label: 'Paskolos vertė nuo turto kainos',
-      description: 'The loan to value of the mortgage',
+        label: 'Paskolos vertė nuo turto kainos',
+        description: 'The loan to value of the mortgage',
     },
     interestRate: {
-      label: 'Palūkanos',
-      description: 'The interest rate of the mortgage',
+        label: 'Paskolos palūkanos (%)',
+        description: 'The annual interest rate of the mortgage',
     },
     mortgageYears: {
-      label: 'Paskolos trukmė',
-      description: 'The number of years the mortgage is for',
+        label: 'Paskolos trukmė',
+        description: 'The number of years the mortgage is for',
     },
     mortgageFees: {
-      label: 'Paskolos mokestis (£)',
-      description: 'The fees associated with the mortgage',
+        label: 'Paskolos mokestis (£)',
+        description: 'The fees associated with the mortgage',
     },
     monthlyRent: {
-      label: 'Mėnesio įmoka',
-      description: 'The monthly rent received from the property',
+        label: 'Nuomos kaina',
+        description: 'The monthly rent received from the property',
     },
     other: {
-      label: 'Kita (£)',
-      description: 'Other income received from the property',
+        label: 'Kita (£)',
+        description: 'Other income received from the property',
     },
     vacantMonths: {
-      label: 'Laisvi mėnesiai (per metus)',
-      description: 'The number of months the property is vacant',
+        label: 'Laisvi mėnesiai (per metus)',
+        description: 'The number of months the property is vacant',
     },
     managementFees: {
-      label: 'Valdymo mokesčiai (£)',
-      description: 'The fees associated with managing the property',
+        label: 'Valdymo mokesčiai (£)',
+        description: 'The fees associated with managing the property',
     },
     solicitorFees: {
-      label: 'Advokato mokesčiai (£)',
-      description: 'The fees associated with the solicitor',
+        label: 'Advokato mokesčiai (£)',
+        description: 'The fees associated with the solicitor',
     },
     annualMaintenance: {
-      label: 'Kasmetinė priežiūra (£)',
-      description: 'The annual maintenance costs',
+        label: 'Kasmetinė priežiūra (£)',
+        description: 'The annual maintenance costs',
     },
     annualServiceCharge: {
-      label: 'Metinis paslaugų mokestis (£)',
-      description: 'The annual service charge',
+        label: 'Metinis paslaugų mokestis (£)',
+        description: 'The annual service charge',
     },
     groundRent: {
-      label: 'Žemės mokestis (£)',
-      description: 'The annual ground rent',
+        label: 'Žemės mokestis (£)',
+        description: 'The annual ground rent',
     }
-  }
-  
+};
